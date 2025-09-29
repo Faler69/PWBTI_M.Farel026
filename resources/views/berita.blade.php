b6677446738b6609608b08bd8473edea2ec149a9
Faler69 <mh.farel140206@gmail.com>
2025-09-29 19:34:20 +0700

    pertama

---
+++ resources/views/berita.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <h1>ini halaman berita</h1>
+<body>
+    <p>Selamat datang di halaman home</p>
+</body>
+</html>
